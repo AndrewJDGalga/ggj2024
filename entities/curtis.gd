7ab6740f260551e70a9a58e49df2f019a87e9168
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-27 09:28:53 -0600

    basic implementation of accuracy

--- entities/curtis.gd
+++ entities/curtis.gd
@@ -3,11 +3,14 @@ extends Area2D
 @onready var lure = $fishing_lure
 @export var game_manager:Game_Manager
 @export var turn_speed := 2.0
-##rotation limit for player's line
+##rotation limit for player's line + accuracy mod
 @export var line_degree_limit := 45.0
 @export var pow_gain_rate := 300.0
+@export var accuracy_gain_rate := 40.0
 var cur_throw_power := 0.0
-var gain_power := true
+var cur_accuracy := 0.0
+#tied to pow and accuracy gain
+var gain_attribute := true
 
 func _physics_process(delta):
 	if game_manager:
@@ -17,26 +20,41 @@ func _physics_process(delta):
 					game_manager.cur_state = game_manager.PLAY_STATE.CASTING
 				turn_for_casting(delta)
 			game_manager.PLAY_STATE.POWER:
-				if gain_power:
-					cur_throw_power -= pow_gain_rate * delta
-				if !gain_power:
-					cur_throw_power += pow_gain_rate * delta
-				if cur_throw_power <= game_manager.lure_y_limit:
-					gain_power = false
-				if cur_throw_power >= 0:
-					gain_power = true
-				print(cur_throw_power)
-				if Input.is_action_pressed("action1"):
-					game_manager.cur_state = game_manager.PLAY_STATE.ACCURACY
-					print(cur_throw_power)
+				set_power(delta)
+			game_manager.PLAY_STATE.ACCURACY:
+				set_accuracy(delta)
 			game_manager.PLAY_STATE.CASTING:
 				pass
 			game_manager.PLAY_STATE.CATCHING:
 				pass
-	
-	#turn_for_casting(delta)
 
+func set_accuracy(delta):
+	if gain_attribute:
+		cur_accuracy += accuracy_gain_rate * delta
+	if !gain_attribute:
+		cur_accuracy -= accuracy_gain_rate * delta
+	if cur_accuracy > line_degree_limit:
+		gain_attribute = false
+	if cur_accuracy < -line_degree_limit:
+		gain_attribute = true
+	print(cur_accuracy)
+	if Input.is_action_pressed("action1"):
+		game_manager.cur_state = game_manager.PLAY_STATE.CASTING
+		print(cur_accuracy)
 
+func set_power(delta):
+	if gain_attribute:
+		cur_throw_power -= pow_gain_rate * delta
+	if !gain_attribute:
+		cur_throw_power += pow_gain_rate * delta
+	if cur_throw_power <= game_manager.lure_y_limit:
+		gain_attribute = false
+	if cur_throw_power >= 0:
+		gain_attribute = true
+	print(cur_throw_power)
+	if Input.is_action_pressed("action1"):
+		game_manager.cur_state = game_manager.PLAY_STATE.ACCURACY
+		print(cur_throw_power)
 
 func turn_for_casting(delta):
 	if Input.is_action_pressed("action1") && rotation_degrees > -45:
