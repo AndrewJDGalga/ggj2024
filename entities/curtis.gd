acc6359d4bc5f6b767b307c4a240ef959e9847c0
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-27 00:24:48 -0600

    state system controlled by game manager. Limit player's rotation

--- entities/curtis.gd
+++ entities/curtis.gd
@@ -1,21 +1,29 @@
 extends Area2D
 
 @onready var lure = $fishing_lure
+@export var game_manager:Game_Manager
 @export var turn_speed := 2.0
 @export var y_limit := -650
-var game_manager
-
-func _ready():
-	game_manager = get_tree().root.get_child(0)
+##rotation limit for player's line
+@export var line_degree_limit := 45.0
 
 func _physics_process(delta):
+	if game_manager:
+		match game_manager.cur_state:
+			game_manager.PLAY_STATE.LINE_UP:
+				turn_for_casting(delta)
+			game_manager.PLAY_STATE.CASTING:
+				pass
+			game_manager.PLAY_STATE.CATCHING:
+				pass
+	
 	#turn_for_casting(delta)
 	
 	if Input.is_action_pressed("action1"):
 		lure.position.y = y_limit
 
 func turn_for_casting(delta):
-	if Input.is_action_pressed("action1"):
-		rotation += turn_speed * delta
-	if Input.is_action_pressed("action2"):
+	if Input.is_action_pressed("action1") && rotation_degrees > -45:
 		rotation -= turn_speed * delta
+	if Input.is_action_pressed("action2") && rotation_degrees < 45:
+		rotation += turn_speed * delta
