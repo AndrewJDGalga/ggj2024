567404e1141d1e9b53d40b524b17e2baf670ea5f
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-27 09:11:38 -0600

    looping power gain behavior

--- entities/curtis.gd
+++ entities/curtis.gd
@@ -3,9 +3,11 @@ extends Area2D
 @onready var lure = $fishing_lure
 @export var game_manager:Game_Manager
 @export var turn_speed := 2.0
-@export var y_limit := -650
 ##rotation limit for player's line
 @export var line_degree_limit := 45.0
+@export var pow_gain_rate := 300.0
+var cur_throw_power := 0.0
+var gain_power := true
 
 func _physics_process(delta):
 	if game_manager:
@@ -14,6 +16,16 @@ func _physics_process(delta):
 				if Input.is_action_just_pressed("action1") && Input.is_action_just_pressed("action2"):
 					game_manager.cur_state = game_manager.PLAY_STATE.CASTING
 				turn_for_casting(delta)
+			game_manager.PLAY_STATE.POWER:
+				if gain_power:
+					cur_throw_power -= pow_gain_rate * delta
+				if !gain_power:
+					cur_throw_power += pow_gain_rate * delta
+				if cur_throw_power <= game_manager.lure_y_limit:
+					gain_power = false
+				if cur_throw_power >= 0:
+					gain_power = true
+				print(cur_throw_power)
 			game_manager.PLAY_STATE.CASTING:
 				pass
 			game_manager.PLAY_STATE.CATCHING:
@@ -22,7 +34,7 @@ func _physics_process(delta):
 	#turn_for_casting(delta)
 	
 	if Input.is_action_pressed("action1"):
-		lure.position.y = y_limit
+		lure.position.y = game_manager.lure_y_limit
 
 func turn_for_casting(delta):
 	if Input.is_action_pressed("action1") && rotation_degrees > -45:
