6f0085ebfa0ad386c7e07c07381049f0eadcaba1
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-26 21:30:55 -0600

    player rotation to line up cast

--- entities/curtis.gd
+++ entities/curtis.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
 @onready var lure = $fishing_lure
+@export var turn_speed := 2.0
 
 func _physics_process(delta):
-	pass
+	turn_for_casting(delta)
+
+func turn_for_casting(delta):
+	if Input.is_action_pressed("action1"):
+		rotation += turn_speed * delta
+	if Input.is_action_pressed("action2"):
+		rotation -= turn_speed * delta
