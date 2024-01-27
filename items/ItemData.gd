914fbabc1720abc2a0504264669fa4f860c56a68
JiRath <garethaguilar@gmail.com>
2024-01-27 16:24:07 -0600

    implement score and rarity to itemdata

--- items/ItemData.gd
+++ items/ItemData.gd
@@ -5,6 +5,8 @@ class_name ItemData
 @export var item_name:String
 @export var item_desc:String
 @export var item_texture:Texture2D
+@export var score:int
+@export var rarity:int
 
 var item_weight:int
 
