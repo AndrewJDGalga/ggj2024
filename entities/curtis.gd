3f3ca4ab466bf1dc77c24576d7573b565aad7e5a
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-27 00:16:44 -0600

    set lure position based on y limit

--- entities/curtis.gd
+++ entities/curtis.gd
@@ -2,13 +2,17 @@ extends Area2D
 
 @onready var lure = $fishing_lure
 @export var turn_speed := 2.0
+@export var y_limit := -650
 var game_manager
 
 func _ready():
 	game_manager = get_tree().root.get_child(0)
 
 func _physics_process(delta):
-	turn_for_casting(delta)
+	#turn_for_casting(delta)
+	
+	if Input.is_action_pressed("action1"):
+		lure.position.y = y_limit
 
 func turn_for_casting(delta):
 	if Input.is_action_pressed("action1"):
