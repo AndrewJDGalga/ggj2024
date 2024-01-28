1677c925ca942fa3c5fd2ea343d21487f8378ae7
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-28 00:21:40 -0600

    added summary screen to testing scene. TODO: scoring

--- score_summary.gd
+++ score_summary.gd
@@ -1,3 +1,14 @@
 extends Control
 
-@onready var scorebox
+@onready var scorebox:Label = $ActualScore 
+
+func _ready():
+	visible = false
+
+func activate_scorescreen(score:int):
+	self.visible = true
+	scorebox.text = str(score)
+
+
+func _on_retry_button_pressed():
+	get_tree().reload_current_scene()
