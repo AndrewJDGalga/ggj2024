fdbca18981f052877cf7416e6b1816732aa46871
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-01-28 11:41:01 -0600

    quick and dirty edits to pool overlap logic to ensure correct behavior

--- components/fishing_zone.gd
+++ components/fishing_zone.gd
@@ -3,14 +3,17 @@ extends Area2D
 @onready var col = $CollisionShape2D
 @onready var anim = $AnimatedSpriteZone
 @onready var anim_player = $AnimationPlayer
+var game_manager:Game_Manager
+var valid:bool = false
 ##Range of difficulty, where 1 is hardest and 3 is easiest
-@export_range(1,5) var ease_rating:int = 1
-@export var base_size:float = 16
+#@export_range(1,5) var ease_rating:int = 1
+#@export var base_size:float = 16
 
 func _ready():
-	col.shape.radius = base_size * ease_rating
-	anim.set_scale(Vector2(ease_rating*2, ease_rating*2))
+	#col.shape.radius = base_size * ease_rating
+	#anim.set_scale(Vector2(ease_rating*2, ease_rating*2))
 	anim.play("default")
+	game_manager = get_tree().root.get_child(0)
 	
 func set_enabled(enabled:bool):
 	col.call_deferred("set_disabled", !enabled)
@@ -18,12 +21,19 @@ func set_enabled(enabled:bool):
 		anim_player.play_backwards("FadeRipple")
 	else:
 		anim_player.play("FadeRipple")
-	
+
+
+func _process(delta):
+	if valid:
+		if game_manager.cur_state == game_manager.PLAY_STATE.CASTING:
+			set_enabled(false)
+			$ZoneCooldown.start()
 
 
 func _on_area_entered(_area):
-	set_enabled(false)
-	$ZoneCooldown.start()
+	valid = true
+	#if game_manager.cur_state == game_manager.PLAY_STATE.CASTING:
+	
 
 
 func _on_timer_timeout():
@@ -32,3 +42,7 @@ func _on_timer_timeout():
 
 func _on_animation_player_animation_finished(_anim_name):
 	pass # Replace with function body.
+
+
+func _on_area_exited(_area):
+	valid = false
